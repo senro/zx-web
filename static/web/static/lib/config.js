499509788c74d90becf3590fbadd1a0f4352b565
senro <passtome@foxmail.com>
2015-09-08 13:10:35 +0800

    update

--- static/web/static/lib/config.js
+++ static/web/static/lib/config.js
@@ -1,7 +1,7 @@
 var protocol = window.location.protocol + '//',
     host = window.location.host,
     apiHost='http://zx.yearn.cc/',
-    baseUrl='file:///D:/senro/senro/app/nativescript/easydecoration-zx.yearn.cc/static/web/';
+    baseUrl='http://zx.yearn.cc/web/';
 
 window.apiHost=apiHost;
 window.baseUrl=baseUrl;
