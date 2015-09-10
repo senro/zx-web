76fd3eac3261ab7fd5cbec721df6752a69b02e23
senro <passtome@foxmail.com>
2015-09-10 20:52:17 +0800

    test

--- static/web/components/login/login.js
+++ static/web/components/login/login.js
@@ -18,31 +18,36 @@ define(function(require, exports, module){
     window.parent.postMessage(require('components/cordova/geolocation/getCurrPos'),'*');
 
     $('.btn-login').click(function(){
-        $.ajax({
-            url:window.apiHost+'login',
-            type:'post',
-            data: $('#loginForm').serialize(),
-            success:function(data){
-                if(data.status==1){
-                    $.ajax({
-                        url:window.apiHost+'getUserInfo',
-                        type:'get',
-                        success:function(data){
-                            if(data.status==1){
-                                window.location.href=window.baseUrl+'/index.html#/main';
-                            }else{
-                                window.location.href=window.baseUrl+'/index.html#/userCenter';
+        if($('#username').val()!=''&&$('#password').val()!=''){
+            $.ajax({
+                url:window.apiHost+'login',
+                type:'post',
+                data: $('#loginForm').serialize(),
+                success:function(data){
+                    if(data.status==1){
+                        $.ajax({
+                            url:window.apiHost+'getUserInfo',
+                            type:'get',
+                            success:function(data){
+                                if(data.status==1){
+                                    window.location.href=window.baseUrl+'/index.html#/main';
+                                }else{
+                                    window.location.href=window.baseUrl+'/index.html#/userCenter';
+                                }
                             }
-                        }
-                    });
+                        });
 
-                }else if(data.status==1001){
-                    window.location.href=window.baseUrl+'/index.html#/register';
-                }else{
-                    alert(data.msg);
+                    }else if(data.status==1001){
+                        window.location.href=window.baseUrl+'/index.html#/register';
+                    }else{
+                        alert(data.msg);
+                    }
                 }
-            }
-        });
+            });
+        }else{
+            alert('请完善信息！');
+        }
+
         return false;
     });
 
