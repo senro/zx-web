c09d37f6ac9237bf524df14952b4c7860777f00f
senro <passtome@foxmail.com>
2015-09-10 20:59:38 +0800

    test

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get('/login', routes.login);
 app.post('/web/register', routes.doRegister);
 app.post('/web/saveUserInfo', routes.doSaveUserInfo);
 app.get('/web/getUserInfo', routes.getUserInfo);
+app.get('/web/getAllUsers', routes.getAllUsers);
 app.post('/web/login', routes.doLogin);
 app.get('/web/logout', routes.logout);
 app.get('/home', routes.home);
