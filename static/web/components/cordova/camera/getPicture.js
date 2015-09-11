df594a4f8a7dd8f9f76ddd86291cebf384737862
senro <passtome@foxmail.com>
2015-09-11 16:47:25 +0800

    test

--- static/web/components/cordova/camera/getPicture.js
+++ static/web/components/cordova/camera/getPicture.js
@@ -7,7 +7,7 @@
  */
 define(function (require, exports, module) {
     module.exports=[
-        "navigator.camera.getPicture(onSuccess, onFail, { quality: 10,",
+        "navigator.camera.getPicture(onSuccess, onFail, { quality: 5,",
             "destinationType: Camera.DestinationType.DATA_URL",
         "});",
 
