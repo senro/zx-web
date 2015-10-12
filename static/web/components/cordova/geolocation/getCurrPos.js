a7d82428271513d3d211c2482a451fb9afb49657
senro <passtome@foxmail.com>
2015-10-12 11:44:44 +0800

    test

--- static/web/components/cordova/geolocation/getCurrPos.js
+++ static/web/components/cordova/geolocation/getCurrPos.js
@@ -14,7 +14,7 @@ define(function (require, exports, module) {
                 "$('#iframe')[0].contentWindow.postMessage('getCurrPos|:|'+JSON.stringify(position),'*');",
             "};",
         "var onError = function(error) {",
-            "alert('code:'+ error.code +',message: ' + error.message);",
+            "//alert('code:'+ error.code +',message: ' + error.message);",
         "}",
 
         "navigator.geolocation.getCurrentPosition(onSuccess, onError);"
