12a8c402d4b05cc0b95c9efbcdd258997fa95693
senro <passtome@foxmail.com>
2015-11-30 11:35:58 +0800

    del to fix conflict

--- static/buy/static/lib/GLOBAL_config.js
+++
@@ -1,7 +0,0 @@
-var protocol = window.location.protocol + '//',
-    host = window.location.host,
-    apiHost='/kaola-delivery-web/',
-    baseUrl = protocol + host +'/buy/';
-
-window.baseUrl = baseUrl;
-window.apiHost = apiHost;
