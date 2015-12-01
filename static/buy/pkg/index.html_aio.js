4c286dbb13cdc8ee8fb66fea3e70ae570f383f64
senro <passtome@foxmail.com>
2015-12-01 23:16:22 +0800

    last

--- static/buy/pkg/index.html_aio.js
+++ static/buy/pkg/index.html_aio.js
@@ -1123,42 +1123,47 @@ define('components/waitBuy/searchToBuy_section/searchToBuy_section', ['spm_modul
                     $thisCancelBtn=$this.find('.goodsBox-corner-circle'),
                     canBeCheck=false;
 
-                $currentSection.find('.compareBar2').find('.btn-goods').each(function(){
-                    var $thisButton=$(this),
-                        thisIndex=$currentSection.find('.compareBar2').find('.btn-goods').index(this);
-                    if(!$thisButton.hasClass('active')){
-                        switch (thisIndex){
-                            case 0:
-                                $thisButton.html(thisTit.substr(0, 2)+'...').addClass('bgColor-blue active');
-                                $thisCheckBtn.addClass('color-blue active').attr('data-btnId','0');
-                                $thisCancelBtn.addClass('bgColor-blue').attr('data-btnId','0');
-                                break;
-                            case 1:
-                                $thisButton.html(thisTit.substr(0, 2)+'...').addClass('bgColor-yellow active');
-                                $thisCheckBtn.addClass('color-yellow active').attr('data-btnId','1');
-                                $thisCancelBtn.addClass('bgColor-yellow').attr('data-btnId','1');
-                                break;
-                            case 2:
-                                $thisButton.html(thisTit.substr(0, 2)+'...').addClass('bgColor-orange active');
-                                $thisCheckBtn.addClass('color-orange active').attr('data-btnId','2');
-                                $thisCancelBtn.addClass('bgColor-orange').attr('data-btnId','2');
-                                break;
-                            case 3:
-                                $thisButton.html(thisTit.substr(0, 2)+'...').addClass('bgColor-skyBlue active');
-                                $thisCheckBtn.addClass('color-skyBlue active').attr('data-btnId','3');
-                                $thisCancelBtn.addClass('bgColor-skyBlue').attr('data-btnId','3');
-                                break;
-                        }
-                        canBeCheck=true;
-                        return false;
-                    }else{
-                        canBeCheck=false;
+                if(!$thisCheckBtn.hasClass('active')){
+                    $currentSection.find('.compareBar2').find('.btn-goods').each(function(){
+                        var $thisButton=$(this),
+                            thisIndex=$currentSection.find('.compareBar2').find('.btn-goods').index(this);
+                        if(!$thisButton.hasClass('active')){
+                            switch (thisIndex){
+                                case 0:
+                                    $thisButton.html(thisTit.substr(0, 2)+'...').addClass('bgColor-blue active');
+                                    $thisCheckBtn.addClass('color-blue active').attr('data-btnId','0');
+                                    $thisCancelBtn.addClass('bgColor-blue').attr('data-btnId','0');
+                                    break;
+                                case 1:
+                                    $thisButton.html(thisTit.substr(0, 2)+'...').addClass('bgColor-yellow active');
+                                    $thisCheckBtn.addClass('color-yellow active').attr('data-btnId','1');
+                                    $thisCancelBtn.addClass('bgColor-yellow').attr('data-btnId','1');
+                                    break;
+                                case 2:
+                                    $thisButton.html(thisTit.substr(0, 2)+'...').addClass('bgColor-orange active');
+                                    $thisCheckBtn.addClass('color-orange active').attr('data-btnId','2');
+                                    $thisCancelBtn.addClass('bgColor-orange').attr('data-btnId','2');
+                                    break;
+                                case 3:
+                                    $thisButton.html(thisTit.substr(0, 2)+'...').addClass('bgColor-skyBlue active');
+                                    $thisCheckBtn.addClass('color-skyBlue active').attr('data-btnId','3');
+                                    $thisCancelBtn.addClass('bgColor-skyBlue').attr('data-btnId','3');
+                                    break;
+                            }
+                            canBeCheck=true;
+                            return false;
+                        }else{
+                            canBeCheck=false;
 
+                        }
+                    });
+                    if(!canBeCheck){
+                        J.showToast('最多只能选择4个商品！','error');
                     }
-                });
-                if(!canBeCheck){
-                    J.showToast('最多只能选择4个商品！','error');
+                }else{
+                    J.showToast('已经选中该商品！','error');
                 }
+
                 return false;
             });
 
@@ -1169,27 +1174,26 @@ define('components/waitBuy/searchToBuy_section/searchToBuy_section', ['spm_modul
                     btnId= Number($.trim($this.attr('data-btnId')));
 
                 if($this.attr('data-btnId')){
-                    console.log(btnId);
                     switch (btnId){
                         case 0:
                             $currentSection.find('.compareBar2').find('.button').eq(btnId).removeClass('bgColor-blue active');
                             $this.removeAttr('data-btnId').removeClass('bgColor-blue');
-                            $thisCheckBtn.removeClass('color-blue');
+                            $thisCheckBtn.removeClass('color-blue active');
                             break;
                         case 1:
                             $currentSection.find('.compareBar2').find('.button').eq(btnId).removeClass('bgColor-yellow active');
                             $this.removeAttr('data-btnId').removeClass('bgColor-yellow');
-                            $thisCheckBtn.removeClass('color-yellow');
+                            $thisCheckBtn.removeClass('color-yellow active');
                             break;
                         case 2:
                             $currentSection.find('.compareBar2').find('.button').eq(btnId).removeClass('bgColor-orange active');
                             $this.removeAttr('data-btnId').removeClass('bgColor-orange');
-                            $thisCheckBtn.removeClass('color-orange');
+                            $thisCheckBtn.removeClass('color-orange active');
                             break;
                         case 3:
                             $currentSection.find('.compareBar2').find('.button').eq(btnId).removeClass('bgColor-skyBlue active');
                             $this.removeAttr('data-btnId').removeClass('bgColor-skyBlue');
-                            $thisCheckBtn.removeClass('color-skyBlue');
+                            $thisCheckBtn.removeClass('color-skyBlue active');
                             break;
                     }
                 }
