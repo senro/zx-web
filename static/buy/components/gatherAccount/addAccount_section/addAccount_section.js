4d4cf39407f3d856cd27e937bb22d5d2425ed02d
senro <passtome@foxmail.com>
2015-12-01 11:15:56 +0800

    update

--- static/buy/components/gatherAccount/addAccount_section/addAccount_section.js
+++ static/buy/components/gatherAccount/addAccount_section/addAccount_section.js
@@ -5,7 +5,7 @@
  * Time: 15:44
  * To change this template use File | Settings | File Templates.
  */
-define('components/gatherAccount/addAccount_section/addAccount_section', ['spm_modules/zepto/0.0.1/zepto', 'spm_modules/jingle/0.0.1/Jingle.debug.qymodify', 'spm_modules/cookie/0.0.1/cookie', 'components/util/App', 'components/util/utilFunctions', 'spm_modules/get-query-string/0.0.1/get-query-string', 'spm_modules/tab/0.0.2/tab', 'spm_modules/template/3.0.0/template'], function (require, exports, module) {
+define('components/gatherAccount/addAccount_section/addAccount_section', ['spm_modules/zepto/0.0.1/zepto', 'spm_modules/jingle/0.0.1/Jingle.debug.qymodify', 'spm_modules/cookie/0.0.1/cookie', 'components/util/App', 'components/util/utilFunctions', 'spm_modules/get-query-string/0.0.1/get-query-string', 'spm_modules/tab/0.0.2/tab', 'spm_modules/radio/0.0.1/radio', 'spm_modules/template/3.0.0/template'], function (require, exports, module) {
     var $=require('spm_modules/zepto/0.0.1/zepto');
     var J=require('spm_modules/jingle/0.0.1/Jingle.debug.qymodify');
     var cookie=require('spm_modules/cookie/0.0.1/cookie');
@@ -13,6 +13,7 @@ define('components/gatherAccount/addAccount_section/addAccount_section', ['spm_m
     var utilFunctions=require('components/util/utilFunctions');
     var getQueryString=require('spm_modules/get-query-string/0.0.1/get-query-string');
     var tab=require('spm_modules/tab/0.0.2/tab');
+    var radio=require('spm_modules/radio/0.0.1/radio');
     var template=require('spm_modules/template/3.0.0/template');
     template.helper('$',$);
     template.helper('decodeURIComponent', decodeURIComponent);
@@ -44,9 +45,40 @@ define('components/gatherAccount/addAccount_section/addAccount_section', ['spm_m
                 }
             });
             $currentSection.find('.search-result').on('tap','.search-result-word',function(){
-                var $this=$(this);
-                $currentSection.find('.tabCont.active').find('.btn-replace').eq(0).css('display','block !important');
+                var $this=$(this),
+                    value= $.trim($this.html());
+                $currentSection.find('.tabCont.active').find('.btn-replace').attr('data-value',value).css('display','block !important');
+                return false;
             });
+            $currentSection.find('.tabCont.active').find('.btn-replace').on('tap',function(){
+                var $this=$(this),
+                    value=$this.attr('data-value'),canSubmit=true;
+                //替换按钮，应该会调接口
+                $currentSection.find('.tabCont.active').find('.btn-selected').each(function(){
+                    var $thisBtn=$(this);
+                    console.log($.trim($thisBtn.html()));
+                    if($.trim($thisBtn.html())==value){
+                        canSubmit=false;
+                        return false;
+                    }
+                });
+                if(canSubmit){
+                    $this.siblings('.btn-selected').html(value);
+                    $currentSection.find('.tabCont.active').find('.btn-replace').css('display','none !important');
+                    $currentSection.find('.search-result').find('.search-result-word').removeClass('color-orange');
+                }else{
+                    J.showToast('已经有该网站，请选择其他网站替换！','error');
+                }
+
+                return false;
+            });
+            //点击body取消替换状态
+            $currentSection.on('tap',function(){
+                $currentSection.find('.tabCont.active').find('.btn-replace').css('display','none !important');
+                $currentSection.find('.search-result').find('.search-result-word').removeClass('color-orange');
+                return false;
+            });
+            radio($currentSection.find('.search-result').find('.search-result-word'),'color-orange');
         }
     });
 
